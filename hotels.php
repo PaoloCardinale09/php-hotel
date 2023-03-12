256bbf580dae8e5797d05a30e043906ae4d7e5a5
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-03-12 23:13:21 +0100

    add bonus

--- hotels.php
+++ hotels.php
@@ -1,6 +1,6 @@
 <?php
 
-    $hotels = [
+      $hotels = [
 
         [
             'name' => 'Hotel Belvedere',
@@ -13,7 +13,7 @@
             'name' => 'Hotel Futuro',
             'description' => 'Hotel Futuro Descrizione',
             'parking' => true,
-            'vote' => 2,
+            'vote' => 3,
             'distance_to_center' => 2
         ],
         [
@@ -40,7 +40,6 @@
 
     ];
 
-    
     // foreach ($hotels as $hotel) {
     //   var_dump($hotel); }
 ?>
@@ -51,41 +50,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-hotel</title>
    
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 </head>
 <body>
+    <h1>
+    <?php echo $_GET["parking"]; ?>
+    
+    </h1>
 
-<div class="container">
+    <div class="container">
 
-    <table class="table ">
-        <thead>
-            <tr class="table-primary text-center">
-                <th scope="col">Nome</th>
-                <th scope="col">Descrizione</th>
-                <th scope="col">Parcheggio</th>
-                <th scope="col">Voto</th>
-                <th scope="col">Distanza dal centro</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-    foreach ($hotels as $hotel){ ;
+
+
+<table class="table">
+    <thead>
+        <tr class="table-primary text-center">
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Parcheggio</th>
+            <th scope="col">Voto</th>
+            <th scope="col">Distanza dal centro</th>  
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($hotels as $hotel) :
+            // Filtra gli hotel in base alla presenza o meno di parcheggio
+            if ($_GET['parking'] !== 'all' && $hotel['parking'] != $_GET['parking']) {
+                continue;
+            }
+
+            if ( $_GET['vote'] !== 'all' && $hotel['vote'] < $_GET['vote']) {
+                continue;
+            }
         ?>
-    <tr class="text-center">
-        <td > <?php echo $hotel["name"] ?> </td>
-        <td > <?php echo $hotel["description"] ?> </td>
-        <td > <?php echo $hotel["parking"] ? "SI" : "NO" ?> </td>
-        <td > <?php echo $hotel["vote"] ?> </td>
-        <td > <?php echo $hotel["distance_to_center"]. " km" ?> </td>
-        
-    </tr>
-    <?php }; ?>
-</tbody>
+            <tr class="text-center">
+                <td> <?php echo $hotel["name"] ?> </td>
+                <td> <?php echo $hotel["description"] ?> </td>
+                <td> <?php echo $hotel["parking"] ? "SI" : "NO" ?> </td>
+                <td> <?php echo $hotel["vote"] ?> </td>
+                <td> <?php echo $hotel["distance_to_center"]. " km" ?> </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
 </table>
 </div>
+
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
